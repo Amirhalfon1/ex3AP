0803596e6d5b82539910bd0376fed2155ca356b7
Amir <halfonamir1@gmail.com>
2017-06-12 17:12:49 +0300

    Single player...

---
+++ ex3/ex3/Controllers/SinglePlayerMazeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ex3.Models;
+
+namespace ex3.Controllers
+{
+    public class SinglePlayerMazeController : ApiController
+    {
+        SinglePlayerMazeManager 
+
+        // GET: api/SinglePlayerMaze
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SinglePlayerMaze/5
+        [HttpGet]
+        public string GenerateMaze(string name, int rows , int cols)
+        {
+            return "value";
+        }
+
+        // POST: api/SinglePlayerMaze
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SinglePlayerMaze/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SinglePlayerMaze/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ex3/ex3/Models/SinglePlayerMazeManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MazeGeneratorLib;
+using MazeLib;
+
+namespace ex3.Models
+{
+    public class SinglePlayerMazeManager
+    {
+        DFSMazeGenerator mazeGenerator;
+
+        public SinglePlayerMazeManager()
+        {
+            mazeGenerator = new DFSMazeGenerator();
+        }
+        /// <summary>
+        /// Generates the maze.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="rows">The rows.</param>
+        /// <param name="cols">The cols.</param>
+        /// <returns>maze</returns>
+        public Maze GenerateMaze(string name, int rows, int cols)
+        {
+            Maze maze = mazeGenerator.Generate(rows, cols);
+            return maze;
+        }
+    }
+}
