87c9a1956dab5934d131d0efa2d3886d7d70d0ce
Amir <halfonamir1@gmail.com>
2017-06-18 15:27:02 +0300

    DB

---
+++ ex3/ex3/Controllers/UserDetailsModelsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ex3.Models;
+
+namespace ex3.Controllers
+{
+    public class UserDetailsModelsController : ApiController
+    {
+        private ex3AppContext db = new ex3AppContext();
+
+        // GET: api/UserDetailsModels
+        public IQueryable<UserDetailsModel> GetUserDetailsModels()
+        {
+            return db.UserDetailsModels;
+        }
+
+        // GET: api/UserDetailsModels/5
+        [ResponseType(typeof(UserDetailsModel))]
+        public IHttpActionResult GetUserDetailsModel(int id)
+        {
+            UserDetailsModel userDetailsModel = db.UserDetailsModels.Find(id);
+            if (userDetailsModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userDetailsModel);
+        }
+
+        // PUT: api/UserDetailsModels/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUserDetailsModel(int id, UserDetailsModel userDetailsModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userDetailsModel.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(userDetailsModel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserDetailsModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/UserDetailsModels
+        [ResponseType(typeof(UserDetailsModel))]
+        public IHttpActionResult PostUserDetailsModel(UserDetailsModel userDetailsModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.UserDetailsModels.Add(userDetailsModel);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = userDetailsModel.Id }, userDetailsModel);
+        }
+
+        // DELETE: api/UserDetailsModels/5
+        [ResponseType(typeof(UserDetailsModel))]
+        public IHttpActionResult DeleteUserDetailsModel(int id)
+        {
+            UserDetailsModel userDetailsModel = db.UserDetailsModels.Find(id);
+            if (userDetailsModel == null)
+            {
+                return NotFound();
+            }
+
+            db.UserDetailsModels.Remove(userDetailsModel);
+            db.SaveChanges();
+
+            return Ok(userDetailsModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserDetailsModelExists(int id)
+        {
+            return db.UserDetailsModels.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ ex3/ex3/Models/UserDetailsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ex3.Models
+{
+    public class UserDetailsModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
